48c49d7f60548875fc0011b03e4bc45fb89e693c
TECH-BOY987 <neerajpremkumar61@gmail.com>
2025-02-03 12:20:23 +0530

    Create reporting_macros.sv

---
+++ UVM/reporting_macros.sv
@@ -0,0 +1,13 @@
+// Code your testbench here
+// or browse Examples
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+module tb;
+initial begin
+  `uvm_info("tb_top","hello world",UVM_MEDIUM);
+end
+endmodule
+
+"""output"""
+
+# KERNEL: UVM_INFO /home/runner/testbench.sv(7) @ 0: reporter [tb_top] hello world
