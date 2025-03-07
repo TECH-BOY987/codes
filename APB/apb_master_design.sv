dd2190477698653fba54d44151d9d982a537118c
TECH-BOY987 <neerajpremkumar61@gmail.com>
2025-03-07 12:43:49 +0530

    .

--- APB/apb_master_design.sv
+++ APB/apb_master_design.sv
@@ -1,50 +1,133 @@
 module apb_master(
 input wire clk,
 input wire rst,
-input wire [31:0] prdata,
-input wire pready, 
-output reg psel,
-output reg pen,
-output reg [31:0] paddr,
-output reg pwrite,
-output reg [31:0] pwdata,
+input wire psel,
+input wire pen,
+input wire [31:0] paddr,
+input wire  [31:0] pwdata,
+input wire [31:0] pwrite,
+output reg [31:0] prdata,
+output reg pready, 
 output reg pslverr
 );
 
-typedef enum logic [1:0]{IDLE,SETUP,ACCESS} state_t;
-state_t state;
-always@(posedge clk or negedge rst) begin
-    if(!rst) begin
-        state<=IDLE;
-        psel<=1'b0;
-        pen<=1'b0;
-    end
-    else begin
-    case(state)
-    IDLE: begin
-        psel<=32'h0000_0000;
-        pwrite<=1'b1;
-        pwdata<=32'h1234_5678;
-        pen<=1'b1;
-        if(pready) begin
-            state<=ACCESS;
+parameter IDLE = 3'b000;
+parameter SETUP = 3'b010;
+parameter ACCESS = 3'b100;
+
+reg [1:0] ps,ns;
+
+reg [31:0] memory [31:0];
+
+always@(posedge clk) begin
+    if(!rst)
+    ps<=IDLE;
+    else
+    ps<=ns;
+end
+always@(*)  begin
+    case(ps)
+    IDLE:
+        if(!psel)
+        ns<=IDLE;
+        else
+        ns<=ACCESS;
+    SETUP:
+        if(psel && pen) begin
+            ns<=ACCESS;
+            pready=1;
+             if (pwrite) begin
+    
+                    memory[paddr] <= pwdata;
+                    pready <= 1'b1;
+                end else begin
+
+                    prdata <= memory[paddr];
+                    pready <= 1'b1;
+                end
         end
-    end
-    SETUP: begin
-        paddr<=32'h0000_0000;
-        pwrite<=1'b1;
-        pwdata<=32'h1234_5678;
-        pen<=1'b1;
-        if(pready) begin
-            state<=ACCESS;
-        end
-    end
-    ACCESS: begin
-      psel<=1'b0;
-      pen<=1'b0;
-      state<=IDLE;
-    end
-    endcase
-    end
+    ACCESS:
+if(!psel&&!pen) begin
+    ns<= IDLE;
+    pready<=0;
+    prdata<=0;
+end
+endcase
+end
+endmodule
+
+//testbench
+
+module tb();
+reg clk=0;
+reg rst;
+reg psel;
+reg pen;
+reg [31:0] paddr;
+reg  [31:0] pwdata;
+reg [31:0] pwrite;
+wire [31:0] prdata;
+wire pready;
+wire pslverr;
+
+apb_master dut(.clk(clk),.rst(rst),.psel(psel),.pen(pen),.paddr(paddr),.pwdata(pwdata),.pwrite(pwrite),.prdata(prdata),.pready(pready),.pslverr(pslverr));
+
+always #5 clk=~clk;
+
+task initialize;
+begin
+    clk=0;
+    rst=0;
+    psel=0;
+    paddr=0;
+    pen=0;
+    pwrite=0;
+end
+endtask
+task reset;
+begin
+    rst=0;
+    #5; rst=1;
+end
+task write();
+begin
+@(posedge clk);
+paddr=paddr+1;
+psel=1;
+pwrite=1;
+pwdata=($random)%10;
+$display("data_in=%0d,addr=%0d",pwdata,paddr);
+@(posedge clk);
+pen=1;
+
+@(posedge clk);
+pen=0;
+psel=0;
+pwdata=0;
+end
+endtask
+task read();
+begin
+@(posedge clk);
+paddr=paddr+1;
+psel=1;
+pwrite=0;
+@(posedge clk);
+pen=1;
+$display("data_out=%0d,addr=%0d",prdata,paddr);
+
+@(posedge clk);
+pen=0;
+psel=0;
+pwdata=0;
+end
+endtask
+initial begin
+    initialize;
+    reset;
+
+    write();
+    paddr=0;
+    read();
 end
 endmodule
